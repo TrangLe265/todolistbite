3c0a6f8e2d9ac2dc82c4b7c2c0bae8c40d068c82
TrangLe265 <trang.le@myy.haaga-helia.fi>
2024-10-08 13:48:46 +0300

    try testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,27 @@
 import App from "./App";
-import { test } from "vitest";
+import { expect, test } from "vitest";
+import {fireEvent, render, screen} from "@testing-library/react"; 
+import '@testing-library/jest-dom/vitest';
 
-test("renders header", () => {});
+test("renders header", () => {
+    render(<App />); 
+    const header = screen.getByText(/My Todolist/i); 
+    //checking if the header is exactly as stated 
+    expect(header).toBeInTheDocument(); 
+});
+
+//test if the Add button works after all fields are filled
+test("add todo", () =>{
+    render(<App />);
+    const desc = screen.getByPlaceholderText("Description");
+    fireEvent.change(desc, {target: {value:"Go to cafe"} }); 
+
+    const date = screen.getByPlaceholderText("Date"); 
+    fireEvent.change(date, {target: {value:"29.01.2023"} }); 
+
+    const button = screen.getByText("Add");
+    fireEvent.click(button); 
+
+    const table = screen.getByRole("table"); 
+    expect(table).toHaveTextContent(/Go to cafe/i); 
+}); 
